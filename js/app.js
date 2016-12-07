78c793510e68a668bc83ea92b7b652982fe5ae6c
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-07 14:57:55 -0800

    hasOwnProperty

--- js/app.js
+++ js/app.js
@@ -12,5 +12,11 @@ require(['jquery', 'MyApp'], function($, MyApp) {
     console.log("schoolName" in school);  // 'in' operator returns true if the specified property is in the specified object
     console.log("schoolType" in school);
     console.log("toString" in school);  // 'toString' method is inherited from Object.prototype
+
+    console.log("--- hasOwnProperty ---");
+
+    var school2 = { schoolName: "MIT" };
+    console.log(school.hasOwnProperty("schoolName"));
+    console.log(school.hasOwnProperty("toString"));  // the inherited 'toString' method is NOT an own property of school
   });
 });
