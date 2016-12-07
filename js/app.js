6944c8b460390e2903db8d6cdc77f5ab523c9b11
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-07 12:22:56 -0800

    Reference data type and primitive data types

--- js/app.js
+++ js/app.js
@@ -6,11 +6,15 @@ require(['jquery', 'MyApp'], function($, MyApp) {
 
     var myApp = new MyApp();
 
+    console.log("--- Simple object ---");
+
     var myFirstObject = {
       firstName: "Richard",
       favoriteAuthor: "Conrad"
     };
     console.dir(myFirstObject);
+
+    console.log("--- Number as a property name and use of bracket notation ---");
 
     var ageGroup = {
       30: "Children",
@@ -18,5 +22,18 @@ require(['jquery', 'MyApp'], function($, MyApp) {
     };
     console.log(ageGroup["30"]);
 
+    console.log("--- Reference data type and primitive data types ---");
+
+    var person = "Kobe";  // Primitive data type String
+    var anotherPerson = person;  // Primitive data saved-as-value (COPIED)
+    person = "Bryant";
+    console.log(anotherPerson);  // Value was NOT modified and retains the original value
+    console.log(person);
+
+    var person2 = { name: "Magic" };  // Non-primitive data
+    var anotherPerson2 = person2;  // Non-primitive data saved-as-reference
+    person2.name = "Jhonson";
+    console.log(anotherPerson2.name);  // Value was changed
+    console.log(person2.name);
   });
 });
