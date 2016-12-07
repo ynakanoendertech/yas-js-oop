04ada52caa7f566ed7592fba51e5afff9d34da6b
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-07 15:57:34 -0800

    Prototype property can be inherited

--- js/app.js
+++ js/app.js
@@ -6,5 +6,20 @@ require(['jquery', 'MyApp'], function($, MyApp) {
 
     var myApp = new MyApp();
 
+    console.log("--- Prototype property ---");
+
+    function PrintStuff(myDocuments) {
+      this.documents = myDocuments;
+    }
+
+    // Add 'print' method so that it is inheritable
+    PrintStuff.prototype.print = function() {
+      console.log(this.documents);
+    };
+
+    // Create a new object and inherit 'print' method from PrintStuff
+    var newObj = new PrintStuff("I am a new Object and I can print");
+    newObj.print();
+
   });
 });
