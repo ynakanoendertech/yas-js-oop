a0ef3539c97cc37d2860a9f0a58506832cb75b55
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-07 15:40:49 -0800

    Serialize and deserialize objects

--- js/app.js
+++ js/app.js
@@ -6,5 +6,23 @@ require(['jquery', 'MyApp'], function($, MyApp) {
 
     var myApp = new MyApp();
 
+    console.log("--- Serialize and deserialize objects ---");
+
+    console.log("- stringify");
+
+    var xmasListObj = { mike: "book", jason: "sweater", chelsea: "iPad" };
+    var xmasListJSONstr = JSON.stringify(xmasListObj);
+    var xmasListJSONstrWithFormat = JSON.stringify(xmasListObj, null, 3);
+    console.dir(xmasListObj);
+    console.log(xmasListJSONstr);
+    console.log(xmasListJSONstrWithFormat);
+
+    console.log("- parse");
+
+    var giftListJSONstr = '{"mike":"iMac","jason":"USB memory stick","chels":"iPad"}';
+    var giftListObj = JSON.parse(giftListJSONstr);
+    console.log(giftListJSONstr);
+    console.dir(giftListObj);
+
   });
 });
