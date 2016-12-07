a490a29195d3a049d389ca9cbe9299dc8cfa6583
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-07 13:24:48 -0800

    Creating objects

--- js/app.js
+++ js/app.js
@@ -6,5 +6,35 @@ require(['jquery', 'MyApp'], function($, MyApp) {
 
     var myApp = new MyApp();
 
+    console.log("--- Creating objects ---");
+
+    // Object literals
+
+    var myBooks = {};
+    console.dir(myBooks);
+
+    var mango = {
+      color: "yello",
+      shape: "round",
+      sweetness: 8,
+      howSweetAmI: function() {
+        console.log( this.sweetness );
+      }
+    };
+    console.dir(mango);
+    mango.howSweetAmI();
+
+    // Object constructor (use the 'new' keyword)
+
+    var orange = new Object();
+    orange.color = "orange";
+    orange.shape = "round";
+    orange.sweetness = 6;
+    orange.howSweetAmI = function() {
+      console.log( this.sweetness );
+    };
+    console.dir(orange);
+    orange.howSweetAmI();
+
   });
 });
