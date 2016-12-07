2c7de6286a9e45901aac3cc7e4d836716f7f949d
Yasuhiro Nakano <ynakano@endertech.com>
2016-12-07 14:55:29 -0800

    Own and inherited properties

--- js/app.js
+++ js/app.js
@@ -6,5 +6,11 @@ require(['jquery', 'MyApp'], function($, MyApp) {
 
     var myApp = new MyApp();
 
+    console.log("--- Own and inherited properties ---");
+
+    var school = { schoolName: "MIT" };
+    console.log("schoolName" in school);  // 'in' operator returns true if the specified property is in the specified object
+    console.log("schoolType" in school);
+    console.log("toString" in school);  // 'toString' method is inherited from Object.prototype
   });
 });
